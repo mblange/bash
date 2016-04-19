552c2bfdfed5f5449f7c6dd5ee327a22065f84f8
mblange <mblange@users.noreply.github.com>
2016-04-19 12:15:22 -0500

    modified wait and idle times

--- bin/portScan.sh
+++ bin/portScan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in {1..65564}; do echo $i; ncat -i 1 <ip.addr> $i; done |& grep -B 2 'Idle'
+for i in {1..65564}; do echo $i; ncat -i .5 -w .5 <ip.addr> $i; done |& grep -B 2 'Idle'
